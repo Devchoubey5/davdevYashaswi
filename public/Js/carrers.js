e9f9f11002671361bc6ba5fdca129353ee75aa2d
Devendra choubey <54746615+Devchoubey5@users.noreply.github.com>
2022-12-28 10:15:26 +0530

    form

---
+++ public/Js/carrers.js
@@ -0,0 +1,32 @@
+const { text } = require("express");
+
+function CheckValidation()
+if (isvalidate){
+    if (jquery('#Name').val() == ' '){
+        getvalidate('Name', 'text', 'candidate name');
+        return false;
+    }
+    if (jquery('#Mobile-no').val() == ' '){
+        getvalidate('Mobile-no', 'number', 'candidate name');
+        return false;
+    }
+    if (jquery('#Message').val() == ' '){
+        getvalidate('Message', 'text', 'textarea');
+        return false;
+    }
+    if (jquery('#Resume-upload').val() == ' '){
+        getvalidate('Resume-upload', 'Document', 'Upload');
+        return false;
+    }
+    else{
+
+        Insupdcarrer();
+    }
+
+}
+
+function Insupdcarrer(){
+    var MasterData ={
+        p
+    }
+}
