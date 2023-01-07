81c3509aa468e0cd935a2588a993a13d209177cf
Devendra choubey <54746615+Devchoubey5@users.noreply.github.com>
2023-01-07 11:58:53 +0530

    .

--- public/Js/carrers.js
+++ public/Js/carrers.js
@@ -1,32 +1,67 @@
-const { text } = require("express");
+function Checkvalidation()
+if (isvalidate) {
 
-function CheckValidation()
-if (isvalidate){
-    if (jquery('#Name').val() == ' '){
-        getvalidate('Name', 'text', 'candidate name');
+    if (jquery('#Name').val() == ' ') {
+        getvalidate('Name', 'text', 'candidate Name');
         return false;
     }
-    if (jquery('#Mobile-no').val() == ' '){
-        getvalidate('Mobile-no', 'number', 'candidate name');
+
+    if (jquery('#contactno').val() == ' ') {
+        getvalidate('contactus', 'number', 'mobileno');
         return false;
     }
-    if (jquery('#Message').val() == ' '){
-        getvalidate('Message', 'text', 'textarea');
+    if (jquery('#Email').val() == ' ') {
+        getvalidate('Email', 'text', 'Email');
         return false;
     }
-    if (jquery('#Resume-upload').val() == ' '){
-        getvalidate('Resume-upload', 'Document', 'Upload');
+
+    if (jquery('#messaage').val() == '') {
+        getvalidate('msg', 'text', 'Message');
         return false;
     }
     else{
 
-        Insupdcarrer();
+        Insupdcareer();
     }
+}
+
+function Insupdcareer() {
+ 
+var MasterData = {
+
+"p_career_Id":'0',
+"p_Name" :jquery("#Name").val(),
+"p_contactno":jquery("#number").val(),
+"p_mail":jquery("#Email").val(),
+"p_Message":jquery("#Message").val(),
+};
+
+jQuery.ajax({
+    url: serverpath + 'career',
+    type: "POST",
+    data: JSON.stringify(MasterData),
+    contentType: "application/json; charset=utf-8",
+    success: function (data, status, jqXHR) {
+        resetMode();
+        if(data[0][0].ReturnValue == "1"){
+            toastr.success("Insert Successful", "", "success")
+            return true;
+        }
+    },
+      error: function (result) {
+       
+        toastr.success(result, "", "success")
+    }
+});
 
 }
 
-function Insupdcarrer(){
-    var MasterData ={
-        p
+function resetMode(){
+
+    $("#btnsubmit").html('submit');
+    jquery("#Name").val("");
+    jQuery("#contactno").val("");
+    jQuery("#Email").val("");
+    jQuery("#Message").val("");
+
     }
-}
